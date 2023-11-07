8b6bb5070c80b0c5d54a45254a97914e470cd303
Fernando Ayats <ayatsfer@gmail.com>
2023-11-07 13:02:51 +0000

    fix devshells

--- haskell/default.nix
+++ haskell/default.nix
@@ -1,35 +1,15 @@
-# let
-#   pkgs = import <nixpkgs> {};
-#   # haskellPackages = pkgs.haskell.packages.ghc92;
-#   haskellPackages = pkgs.haskellPackages;
-# in
-#   haskellPackages.shellFor {
-#     packages = p: [
-#     ];
-#     buildInputs = [
-#       pkgs.zlib
-#       haskellPackages.haskell-language-server
-#       (pkgs.symlinkJoin {
-#         inherit (pkgs.stack) name pname version;
-#         paths = [pkgs.stack];
-#         nativeBuildInputs = [pkgs.makeWrapper];
-#         postBuild = ''
-#           wrapProgram "$out/bin/stack" \
-#             --add-flags "--no-nix" \
-#             --add-flags "--system-ghc" \
-#             --add-flags "--no-install-ghc"
-#         '';
-#       })
-#     ];
-#   }
 let
   pkgs = import <nixpkgs> {};
+  haskellPackages = pkgs.haskellPackages;
 in
-  pkgs.mkShell {
-    packages = [
-      pkgs.zlib
-      pkgs.stack
-      (pkgs.haskell-language-server.override {supportedGhcVersions = ["927"];})
-      pkgs.exercism
+  haskellPackages.shellFor {
+    packages = p: [
     ];
+    buildInputs = [
+      pkgs.zlib
+      haskellPackages.haskell-language-server
+      pkgs.cabal-install
+      pkgs.exercism
+      pkgs.hpack
+          ];
   }
