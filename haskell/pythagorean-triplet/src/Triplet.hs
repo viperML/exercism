7e5751d0e6e5d07c1acf79b22d57434f931dc364
Fernando Ayats <ayatsfer@gmail.com>
2023-11-13 23:05:29 +0000

    init scm

--- haskell/pythagorean-triplet/src/Triplet.hs
+++ haskell/pythagorean-triplet/src/Triplet.hs
@@ -1,4 +1,31 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Triplet (tripletsWithSum) where
 
+import Control.Parallel.Strategies (Eval)
+import Data.Function ((&))
+import Debug.Trace (trace, traceId, traceShow, traceShowId)
+import Prelude hiding (sum)
+
 tripletsWithSum :: Int -> [(Int, Int, Int)]
-tripletsWithSum sum = error "You need to implement this function."
+tripletsWithSum sum = do
+    !c <- candidates sum
+
+    []
+
+candidates :: Int -> [(Int, Int, Int)]
+candidates sum = do
+    a <- [1 .. sum]
+    b <- [1 .. sum - a]
+    let c = traceShowId $ sum - a - b
+
+    if c < a
+        then return (c, b, a)
+        else
+            if c < b
+                then return (a, c, b)
+                else return (a, b, c)
+
+-- if c > 0
+--     then return (a, b, c)
+--     else []
